ff7ac0ec030e3cef9bf7acb8267cef0f6c3749ce
alex.collins <alex.collins@gamesys.co.uk>
2015-12-24 20:46:59 +0000

    blue ghost

--- public/javascripts/Arena.js
+++ public/javascripts/Arena.js
@@ -180,8 +180,9 @@ Arena.prototype.draw = function(ctx) {
 
       var numFrames = player.type === 0 ? 3 : 2;
       var frame = (player.x + player.y) % numFrames;
-      var origin = {0: 0, 1: 16, 2: 24, 3: 32, 4: 40}[player.type];
-      var offset = Math.max('udlr'.indexOf(player.direction) * numFrames, 0);
+      var vulnerable = this.pillTimeLeft > 0 && player.type > 0;
+      var origin = vulnerable ? 12 : {0: 0, 1: 16, 2: 24, 3: 32, 4: 40}[player.type];
+      var offset = vulnerable ? 0 : Math.max('udlr'.indexOf(player.direction) * numFrames, 0);
       var i = origin + offset + frame;
       var x = i % 8;
       var y = parseInt(i / 8);
